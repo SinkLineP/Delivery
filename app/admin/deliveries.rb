f0e34a39626a0438cedda05a10cc680b1bcb6ac0
sinkline <popovartem505@gmail.com>
2024-01-27 11:22:54 +0300

    Delivery admin panel

--- app/admin/deliveries.rb
+++ app/admin/deliveries.rb
@@ -1,18 +1,58 @@
 ActiveAdmin.register Delivery do
+  permit_params :name, :surname, :lastname, :phone, :email, :weight, :length, :width, :height, :price, :distance, :departure_point, :destination
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :surname, :lastname, :phone, :email, :weight, :length, :width, :height, :price, :distance, :departure_point, :destination
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :surname, :lastname, :phone, :email, :weight, :length, :width, :height, :price, :distance, :departure_point, :destination]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :surname
+    column :lastname
+    column :phone
+    column :email
+    column :weight
+    column :length
+    column :width
+    column :height
+    column :price
+    column :distance
+    column :departure_point
+    column :destination
+    column :created_at
+    actions
+  end
+
+  filter :name
+  filter :surname
+  filter :lastname
+  filter :phone
+  filter :email
+  filter :weight
+  filter :length
+  filter :width
+  filter :height
+  filter :price
+  filter :distance
+  filter :departure_point
+  filter :destination
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :surname
+      f.input :lastname
+      f.input :phone
+      f.input :email
+      f.input :weight
+      f.input :length
+      f.input :width
+      f.input :height
+      f.input :price
+      f.input :distance
+      f.input :departure_point
+      f.input :destination
+    end
+    f.actions
+  end
   
 end
